1ef81e4b35244f54796a7f1814b773e3053228d7
shrinivas <shrinivasmalali3@gmail.com>
2026-02-25 16:01:02 +0530

    Updated message v2

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Docker + Jenkins CI/CD!");
+        System.out.println("Version 2 deployed from CI/CD 🚀");
     }
 }
